da7a5354969bb241bbac6f232857650965e55bd0
Andrey <asvidovskiy@gmail.com>
2022-06-10 09:02:34 +0300

    change

--- server.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-//робимо щоб php міг працювати з JSON форматом
-$_POST = json_decode(file_get_contents("php://input"), true);
-
-//виводимо те що прийшло з методом POST в консоль
-echo var_dump($_POST);
