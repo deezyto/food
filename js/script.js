afd9a31a3fe73657c75814e23af38688a9ef6884
Andrey <asvidovskiy@gmail.com>
2022-06-15 17:14:39 +0300

    update calculator

--- js/script.js
+++ js/script.js
@@ -638,9 +638,22 @@ fetch('http://localhost:3000/menu')
 //робимо калькулятор на сайті
 
 const calculatingResult = document.querySelector('.calculating__result span');
-let sex = 'female', 
-    height, weight, age, 
-    ratio = '1.375';
+
+let sex, height, weight, age, ratio;
+
+if (localStorage.getItem('sex')) {
+  let sex = localStorage.getItem('sex');
+} else {
+  sex = 'female';
+  localStorage.setItem('sex', 'female');
+}
+
+if (localStorage.getItem('ratio')) {
+  let ratio = localStorage.getItem('ratio');
+} else {
+  ratio = 1.375;
+  localStorage.setItem('ratio', '1.375');
+}
 
 function calculating() {
   if (!sex || !height || !weight || !age || !ratio) {
@@ -654,16 +667,36 @@ function calculating() {
   }
 }
 
+function initLocalSetting(selector, activeClass) {
+  const elements = document.querySelectorAll(selector);
+
+  elements.forEach(elem => {
+    elem.classList.remove(activeClass);
+    if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+      elem.classList.add(activeClass);
+    }
+
+    if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+      elem.classList.add(activeClass);
+    }
+  });
+}
+
+initLocalSetting('#gender div', 'calculating__choose-item_active');
+initLocalSetting('.calculating__choose_big div', 'calculating__choose-item_active');
+
 calculating();
-function getStaticInfo(parentSelector, activeClass) {
-  const elements = document.querySelectorAll(`${parentSelector} div`);
+function getStaticInfo(selector, activeClass) {
+  const elements = document.querySelectorAll(selector);
 
   elements.forEach(elem => {
     elem.addEventListener('click', (e) => {
       if (e.target.getAttribute('data-ratio')) {
         ratio = +e.target.getAttribute('data-ratio');
+        localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
       } else {
         sex = e.target.getAttribute('id');
+        localStorage.setItem('sex', e.target.getAttribute('id'));
       }
       console.log(ratio, sex);
       elements.forEach(elem => {
@@ -675,32 +708,22 @@ function getStaticInfo(parentSelector, activeClass) {
       calculating();
     });
   });
-  /* варіант з делегуванням подій
-  document.querySelector(parentSelector).addEventListener('click', (e) => {
-    if (e.target.getAttribute('data-ratio')) {
-      ratio = +e.target.getAttribute('data-ratio');
-    } else {
-      sex = e.target.getAttribute('id');
-    }
-    console.log(ratio, sex);
-    elements.forEach(elem => {
-      elem.classList.remove(activeClass);
-    });
-
-    e.target.classList.add(activeClass);
-
-    calculating();
-  });
-  */
 }
 
-getStaticInfo('#gender', 'calculating__choose-item_active');
-getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+getStaticInfo('#gender div', 'calculating__choose-item_active');
+getStaticInfo('.calculating__choose_big div', 'calculating__choose-item_active');
 
 function getDynamicInfo(selector) {
   const input = document.querySelector(selector);
 
   input.addEventListener('input', () => {
+
+    if(input.value.match(/\D/g)) {
+      input.style.border = '1px solid red';
+    } else {
+      input.style.border = 'none';
+    }
+
     switch(input.getAttribute('id')) {
       case 'height':
         height = +input.value;
