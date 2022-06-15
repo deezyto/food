2782fa382652d79049ccae2fcebf0cee755ec784
Andrey <asvidovskiy@gmail.com>
2022-06-15 16:45:35 +0300

    create dynamic calculator

--- js/script.js
+++ js/script.js
@@ -635,5 +635,88 @@ fetch('http://localhost:3000/menu')
 .then(data => data.json())
 .then(res => console.log(res));
 
-//
+//робимо калькулятор на сайті
+
+const calculatingResult = document.querySelector('.calculating__result span');
+let sex = 'female', 
+    height, weight, age, 
+    ratio = '1.375';
+
+function calculating() {
+  if (!sex || !height || !weight || !age || !ratio) {
+    calculatingResult.textContent = '0';
+    return;
+  }
+  if (sex === 'female') {
+    calculatingResult.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+  } else {
+    calculatingResult.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+  }
+}
+
+calculating();
+function getStaticInfo(parentSelector, activeClass) {
+  const elements = document.querySelectorAll(`${parentSelector} div`);
+
+  elements.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+      if (e.target.getAttribute('data-ratio')) {
+        ratio = +e.target.getAttribute('data-ratio');
+      } else {
+        sex = e.target.getAttribute('id');
+      }
+      console.log(ratio, sex);
+      elements.forEach(elem => {
+        elem.classList.remove(activeClass);
+      });
+  
+      e.target.classList.add(activeClass);
+  
+      calculating();
+    });
+  });
+  /* варіант з делегуванням подій
+  document.querySelector(parentSelector).addEventListener('click', (e) => {
+    if (e.target.getAttribute('data-ratio')) {
+      ratio = +e.target.getAttribute('data-ratio');
+    } else {
+      sex = e.target.getAttribute('id');
+    }
+    console.log(ratio, sex);
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+    });
+
+    e.target.classList.add(activeClass);
+
+    calculating();
+  });
+  */
+}
+
+getStaticInfo('#gender', 'calculating__choose-item_active');
+getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+
+function getDynamicInfo(selector) {
+  const input = document.querySelector(selector);
+
+  input.addEventListener('input', () => {
+    switch(input.getAttribute('id')) {
+      case 'height':
+        height = +input.value;
+        break;
+      case 'weight':
+        weight = +input.value;
+        break;
+      case 'age':
+        age = +input.value;
+        break;
+    }
+    calculating();
+  });
+}
+
+getDynamicInfo('#height');
+getDynamicInfo('#weight');
+getDynamicInfo('#age');
 });
